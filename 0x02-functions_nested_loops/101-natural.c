9aaaa37df140cab0fedf1efd5b3af9932556d409
alli-cpp <alli-cpp@github.com>
2022-04-13 05:25:31 -0700

    Nature made the natural numbers; All else is the work of women

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,8 +9,10 @@
 int main(void)
 {
 	int 1, sum = 0;
+		
 	for (i = 0; i < 1024; i++)
-	{if ((i % 3) == 0 || (i % 5) ==0)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
 		sum += i;
 	}
 	printf("%d\n", sum);
