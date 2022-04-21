178059568dd106e12afdcfd22dbe9d8f903c8e68
alli-cpp <alli-cpp@github.com>
2022-04-21 13:23:38 -0700

    task0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,14 +7,13 @@
  */
 char *_strcat(char *dest, char *src)
 {
-  int x, y;
-
-  for (x = 0; dest[x] != '\0'; x++);
-  for (y = 0; src[y] != '\0'; y++)
-  {
-    dest[x] = src[y];
-    x++;
-  }
-  dest[x] = '\0';
-  return (dest);
+	int x, y;
+	for (x = 0; dest[x] != '\0'; x++);
+	for (y = 0; src[y] != '\0'; y++)
+	{
+		dest[x] = src[y];
+		x++;
+	}
+	dest[x] = '\0';
+	return (dest);
 }
