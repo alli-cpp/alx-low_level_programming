a4a6ab135924b7f5919f5028a328449eff99c32e
alli-cpp <alli-cpp@github.com>
2022-05-09 12:40:16 -0700

    task 3 structures teypedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -5,12 +5,21 @@
  * @name: The name of the dog
  * @age: The age of the dog
  * @owner: The owner of the dog
+ *
+ * Description: Longer description
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
+#endif
+#ifndef _FUNCTIONS_H
+#define _FUNCTIONS_H
+int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
