2ec7ea1ce129a8330c3c7958fe90c7679123f177
alli-cpp <alli-cpp@github.com>
2022-05-19 12:05:34 -0700

    task 3 singly linked lists

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,38 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "lists.h"
-#include <string.h>
 /**
- * add_node_end - a function that adds a node at the end of a list
- * @head: the head node
- * @str: the value of str
- * Return: a  pointer at that was newly added
+ * add_node_end - a function that adds a new node
+ * at the end of a list_t list.
+ * @head: input
+ * @str: input
+ * Return: 0
  */
-
 list_t *add_node_end(list_t **head, const char *str)
 {
-	unsigned int count = 0;
-	list_t *nod, *temp;
+	int i, n = 0;
+	list_t *new;
+	list_t *temp;
 
-	if (str == NULL)
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
 		return (NULL);
-	while (str[count])
-		count++;
-	nod = malloc(sizeof(list_t));
-	temp = malloc(sizeof(list_t));
-	if (!(nod && temp))
-		return (NULL);
-	nod->str = strdup(str);
-	nod->len = count;
-	nod->next = NULL;
+
+	for (i = 0; str[i] != '\0'; i++)
+		n++;
+
+	new->len = i;
+	new->str = strdup(str);
+	new->next = NULL;
+
 	if (*head == NULL)
 	{
-		*head = nod;
-		return (nod);
+		*head = new;
+		return (new);
 	}
+
 	temp = *head;
-	while (temp->next)
-	{
+
+	while (temp->next != NULL)
 		temp = temp->next;
-	}
-	temp->next = nod;
-	return (nod);
+
+	temp->next = new;
+
+	return (new);
 }
