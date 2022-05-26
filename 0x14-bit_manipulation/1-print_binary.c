cadc8fdc66c516fb6021808c285464fb805518c0
alli-cpp <alli-cpp@github.com>
2022-05-26 13:52:55 -0700

    task 1 bit manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,27 +1,22 @@
 #include "main.h"
 
 /**
- * print_binary - a function that prints the binary representation
- * @n: the number to be printed
+ * print_binary - prints the binary representation
+ * of a number.
+ * @n: unsigned long int.
+ *
+ * Return: no return.
  */
 void print_binary(unsigned long int n)
 {
-	int cc = 0;
-	unsigned int num;
-
-	if (n == 0)
+	if (n >> 0)
+	{
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+	else
 	{
 		_putchar('0');
-		return;
-	}
-	for (num = 1 << 31; num > 0; num = num >> 1)
-	{
-		if (n & num)
-		{
-			_putchar('1');
-			cc++;
-		}
-		else if ((!(n & num)) && cc)
-			_putchar('0');
 	}
 }
