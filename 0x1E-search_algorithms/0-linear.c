d7367a6b48690e208995ef65f03aef3bfbf8b913
alli-cpp <alli-cpp@github.com>
2022-11-18 11:24:40 -0800

    task 0 linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,26 +1,32 @@
 #include "search_algos.h"
 
 /**
- * linear_search - searches for a value in an array of
- * integers using the Linear search algorithm
+ * linear_search - Searches for a value in an array of integers
+ * using the Linear search algorithm
  *
- * @array: input array
- * @size: size of the array
- * @value: value to search in
- * Return: Always EXIT_SUCCESS
+ * @array: A pointer to the first element of the array to search in
+ * @size: The number of elements in array
+ * @value: The value ot  search for
+ *
+ * Return: -1 if the value is not present in the array or if the array
+ * is null, otherwise it returns the first index where the value is
+ * presented.
+ *
  */
+
 int linear_search(int *array, size_t size, int value)
 {
-	int i;
+	size_t i = 0;
 
 	if (array == NULL)
 		return (-1);
 
-	for (i = 0; i < (int)size; i++)
+	while (i < size)
 	{
-		printf("Value checked array[%u] = [%d]\n", i, array[i]);
-		if (value == array[i])
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
 			return (i);
+		i++;
 	}
 	return (-1);
 }
